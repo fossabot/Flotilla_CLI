00a0307ed33cd713f88843559beb0ffad7849275
Ximidar <ximidar@gmail.com>
2018-06-16 17:13:50 -0700

    Added hello world cli interface

--- cmd/cli_interface.go
+++ cmd/cli_interface.go
@@ -2,14 +2,16 @@
 * @Author: Ximidar
 * @Date:   2018-06-16 16:39:58
 * @Last Modified by:   Ximidar
-* @Last Modified time: 2018-06-16 16:54:19
+* @Last Modified time: 2018-06-16 17:13:08
 */
 
 package cmd
 
 import(
 	"fmt"
+	"log"
 	"github.com/spf13/cobra"
+	"github.com/jroimartin/gocui"
 
 )
 
@@ -23,6 +25,51 @@ var printerface = &cobra.Command{
   Long:  `This will open the cli UI for MangoOS. This has tools for monitoring the command line and starting prints (or it will in the future)`,
   Run: func(cmd *cobra.Command, args []string) {
     fmt.Println("Hello, I work!")
+    screen_init()
   },
 }
 
+type Cli_Gui struct{
+	Printer_Name string
+
+}
+
+func New_Cli_Gui() *Cli_Gui {
+	cgui := new(Cli_Gui)
+	cgui.Printer_Name = "Test this var"
+	return cgui
+}
+
+func screen_init() {
+	g, err := gocui.NewGui(gocui.OutputNormal)
+	if err != nil {
+		log.Panicln(err)
+	}
+	defer g.Close()
+
+	g.SetManagerFunc(layout)
+
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		log.Panicln(err)
+	}
+
+	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
+		log.Panicln(err)
+	}
+}
+
+func layout(g *gocui.Gui) error {
+	maxX, maxY := g.Size()
+	if v, err := g.SetView("hello", maxX/2-7, maxY/2, maxX/2+7, maxY/2+2); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		fmt.Fprintln(v, "Hello world!")
+	}
+	return nil
+}
+
+func quit(g *gocui.Gui, v *gocui.View) error {
+	return gocui.ErrQuit
+}
+ 
--- cmd/rootCmd.go
+++ cmd/rootCmd.go
@@ -2,7 +2,7 @@
 * @Author: Ximidar
 * @Date:   2018-06-16 16:53:05
 * @Last Modified by:   Ximidar
-* @Last Modified time: 2018-06-16 16:55:42
+* @Last Modified time: 2018-06-16 17:05:26
 */
 
 package cmd
@@ -16,8 +16,10 @@ import(
 
 var rootCmd = &cobra.Command{
   Use:   "mango_cli",
-  Short: "MangoCLI is the cli tool for MangoOS",
-  Long: `Fill this out later`,
+  Short: "Mango_cli is the cli tool for MangoOS",
+  Long: `Use this tool to control MangoOS from the command line
+  		 This tool will help you print files, check the status of
+  		 a print, or help you control and monitor the printer command line`,
   Run: func(cmd *cobra.Command, args []string) {
     // Do Stuff Here
   },
