567c70795fd772218e73529e548f96da5ef0b47b
Ximidar <ximidar@gmail.com>
2018-09-15 19:58:48 -0700

    First nats message sent success!

--- mango_interface/mango_interface.go
+++ mango_interface/mango_interface.go
@@ -2,118 +2,174 @@
 * @Author: Ximidar
 * @Date:   2018-08-25 10:12:08
 * @Last Modified by:   Ximidar
-* @Last Modified time: 2018-08-26 14:51:01
+* @Last Modified time: 2018-09-15 19:54:28
  */
 
 package mango_interface
 
 import (
-	"errors"
-	"fmt"
-	"github.com/godbus/dbus"
-	"os"
+	_"errors"
+	_"fmt"
+	ms "github.com/ximidar/mango_structures"
+	"github.com/nats-io/go-nats"
+	_"os"
+	"time"
+	"log"
+	"encoding/json"
+)
+
+const(
+	// address name
+	NAME = "commango."
+
+	// reply subs
+	LIST_PORTS = NAME + "list_ports"
+	INIT_COMM = NAME + "init_comm"
+	CONNECT_COMM = NAME + "connect_comm"
+	DISTCONNECT_COMM = NAME + "disconnect_comm"
+	WRITE_COMM = NAME + "write_comm"
+
+	// pubs
+	READ_LINE = NAME + "read_line"
 )
 
 type Mango struct {
-	Conn     *dbus.Conn
-	Comm_Obj dbus.BusObject
+	NC *nats.Conn
+	
 }
+
+/*
+Basic structure of requesting different things
+subj, payload := args[0], []byte(args[1])
+
+msg, err := nc.Request(subj, []byte(payload), 100*time.Millisecond)
+if err != nil {
+	if nc.LastError() != nil {
+		log.Fatalf("Error in Request: %v\n", nc.LastError())
+	}
+	log.Fatalf("Error in Request: %v\n", err)
+}
+log.Printf("Published [%s] : '%s'\n", subj, payload)
+log.Printf("Received [%v] : '%s'\n", msg.Subject, string(msg.Data))
+*/
 
 func NewMango() (*Mango, error) {
 	mgo := new(Mango)
 	var err error
-	mgo.Conn, err = dbus.SessionBus()
+	mgo.NC, err = nats.Connect(nats.DefaultURL)
+
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "Failed to connect to session bus:", err)
+		log.Fatalf("Can't connect: %v\n", err)
 		return nil, err
 	}
-
-	mgo.Comm_Obj = mgo.Conn.Object("com.mango_core.commango", "/com/mango_core/commango")
 
 	return mgo, nil
 }
 
-func (mgo *Mango) Get_Comm_Signal() (chan *dbus.Signal, error) {
-	matchstr := "type='signal',sender='com.mango_core.commango',interface='com.mango_core.commango',path='/com/mango_core/commango'"
-	mgo.Conn.BusObject().Call("org.freedesktop.DBus.AddMatch", 0, matchstr)
-	c := make(chan *dbus.Signal, 10)
-	mgo.Conn.Signal(c)
+func (mgo *Mango) Make_Request(subject string, payload []byte) ([]byte, error){
 
-	return c, nil
+	msg, err := mgo.NC.Request(subject, payload, 100*time.Millisecond)
+
+	if err != nil{
+		panic(err) // TODO make some sort of intelligent way to parse errors
+	}
+
+	return msg.Data, nil
+
 }
+
+// func (mgo *Mango) Get_Comm_Signal() (chan *dbus.Signal, error) {
+// 	c := make(chan string, 10)
+// 	return c, nil
+// }
 
 func (mgo *Mango) Comm_Set_Connection_Options(port string, baud int32) error {
-	call := mgo.Comm_Obj.Call("com.mango_core.commango.Init_Comm", 0, port, baud)
 
-	if call.Err != nil {
-		return call.Err
+	init_comm := new(ms.Init_Comm)
+	init_comm.Port = port
+	init_comm.Baud = int(baud)
+	payload, _ := json.Marshal(init_comm)
+	call, err := mgo.Make_Request(INIT_COMM, payload)
+
+	if err != nil {
+		return err
 	}
+
+	response := new(ms.Reply_String)
+	err = json.Unmarshal(call, response)
+
+	if err != nil{
+		panic(err)
+	}
+
+	log.Printf("Success: %v\nResponse: %v\n", response.Success, response.Message)
+
 	return nil
 
 }
 
-func (mgo *Mango) Comm_Connect() error {
-	call := mgo.Comm_Obj.Call("com.mango_core.commango.Open_Comm", 0)
+// func (mgo *Mango) Comm_Connect() error {
+// 	call := mgo.Comm_Obj.Call("com.mango_core.commango.Open_Comm", 0)
 
-	if call.Err != nil {
-		return call.Err
-	}
-	return nil
-}
+// 	if call.Err != nil {
+// 		return call.Err
+// 	}
+// 	return nil
+// }
 
-func (mgo *Mango) Comm_Disconnect() error {
-	call := mgo.Comm_Obj.Call("com.mango_core.commango.Close_Comm", 0)
+// func (mgo *Mango) Comm_Disconnect() error {
+// 	call := mgo.Comm_Obj.Call("com.mango_core.commango.Close_Comm", 0)
 
-	if call.Err != nil {
-		return call.Err
-	}
-	return nil
+// 	if call.Err != nil {
+// 		return call.Err
+// 	}
+// 	return nil
 
-}
+// }
 
-//Call(method string, flags Flags, args ...interface{}) *Call
-func (mgo *Mango) Comm_Get_Available_Ports() ([]string, error) {
+// //Call(method string, flags Flags, args ...interface{}) *Call
+// func (mgo *Mango) Comm_Get_Available_Ports() ([]string, error) {
 
-	call := mgo.Comm_Obj.Call("com.mango_core.commango.Get_Available_Ports", 0)
+// 	call := mgo.Comm_Obj.Call("com.mango_core.commango.Get_Available_Ports", 0)
 
-	if call.Err != nil {
-		return nil, call.Err
-	}
+// 	if call.Err != nil {
+// 		return nil, call.Err
+// 	}
 
-	if len(call.Body) > 0 {
-		ports, ok := call.Body[0].([]string)
-		if !ok {
-			return nil, errors.New("Could not convert body to []string")
-		}
-		return ports, nil
-	}
+// 	if len(call.Body) > 0 {
+// 		ports, ok := call.Body[0].([]string)
+// 		if !ok {
+// 			return nil, errors.New("Could not convert body to []string")
+// 		}
+// 		return ports, nil
+// 	}
 
-	return []string{}, nil
+// 	return []string{}, nil
 
-}
+// }
 
-func (mgo *Mango) Comm_Write(command string) error {
-	expected_bytes := len(command)
-	call := mgo.Comm_Obj.Call("com.mango_core.commango.Write_Comm", 0, command)
+// func (mgo *Mango) Comm_Write(command string) error {
+// 	expected_bytes := len(command)
+// 	call := mgo.Comm_Obj.Call("com.mango_core.commango.Write_Comm", 0, command)
 
-	if call.Err != nil {
-		return call.Err
-	}
+// 	if call.Err != nil {
+// 		return call.Err
+// 	}
 
-	if len(call.Body) > 0 {
-		bytes_written, ok := call.Body[0].(int)
+// 	if len(call.Body) > 0 {
+// 		bytes_written, ok := call.Body[0].(int)
 
-		if !ok {
-			return errors.New("Could not cast body to int")
-		}
+// 		if !ok {
+// 			return errors.New("Could not cast body to int")
+// 		}
 
-		if bytes_written != expected_bytes {
-			return errors.New("expected_bytes != written bytes")
-		}
+// 		if bytes_written != expected_bytes {
+// 			return errors.New("expected_bytes != written bytes")
+// 		}
 
-	} else {
-		return errors.New("Call did not return any bytes")
-	}
+// 	} else {
+// 		return errors.New("Call did not return any bytes")
+// 	}
 
-	return nil
-}
+// 	return nil
+// }
--- mango_interface/mango_interface_test.go
+++ mango_interface/mango_interface_test.go
@@ -2,31 +2,31 @@
 * @Author: Ximidar
 * @Date:   2018-08-25 10:51:03
 * @Last Modified by:   Ximidar
-* @Last Modified time: 2018-08-25 11:55:47
+* @Last Modified time: 2018-09-15 19:55:00
  */
 package mango_interface_test
 
 import (
-	"fmt"
+	_"fmt"
 	"github.com/ximidar/mango_cli/mango_interface"
 	"testing"
-	"time"
+	_"time"
 )
 
 func Test_Get_Available_Ports(t *testing.T) {
-	fmt.Println("Testing Get Available Ports")
-	mgo, err := mango_interface.NewMango()
+	// fmt.Println("Testing Get Available Ports")
+	// mgo, err := mango_interface.NewMango()
 
-	if err != nil {
-		t.Fatal(err)
-	}
-	ports, err := mgo.Comm_Get_Available_Ports()
-	if err != nil {
-		fmt.Println("Could not get available ports", err)
-		t.Fatal(err)
-	}
+	// if err != nil {
+	// 	t.Fatal(err)
+	// }
+	// ports, err := mgo.Comm_Get_Available_Ports()
+	// if err != nil {
+	// 	fmt.Println("Could not get available ports", err)
+	// 	t.Fatal(err)
+	// }
 
-	fmt.Println(ports)
+	// fmt.Println(ports)
 }
 
 func Test_Comm_set_up_and_write(t *testing.T) {
@@ -37,35 +37,35 @@ func Test_Comm_set_up_and_write(t *testing.T) {
 	}
 
 	mgo.Comm_Set_Connection_Options("/dev/ttyACM0", 115200)
-	mgo.Comm_Connect()
+	// mgo.Comm_Connect()
 
-	duration := time.Duration(5 * time.Second)
-	time.Sleep(duration)
+	// duration := time.Duration(5 * time.Second)
+	// time.Sleep(duration)
 
-	read_chan, err := mgo.Get_Comm_Signal()
-	stop_reading := false
+	// read_chan, err := mgo.Get_Comm_Signal()
+	// stop_reading := false
 
-	read_func := func() {
-		for read := range read_chan {
-			if stop_reading {
-				break
-			}
+	// read_func := func() {
+	// 	for read := range read_chan {
+	// 		if stop_reading {
+	// 			break
+	// 		}
 
-			fmt.Printf("%s", read.Body[0])
-		}
-	}
+	// 		fmt.Printf("%s", read.Body[0])
+	// 	}
+	// }
 
-	go read_func()
+	// go read_func()
 
-	pause_dur := time.Duration(100 * time.Millisecond)
-	writes := []string{"Hello!", "My", "Name", "Is", "Matt"}
+	// pause_dur := time.Duration(100 * time.Millisecond)
+	// writes := []string{"Hello!", "My", "Name", "Is", "Matt"}
 
-	for _, write := range writes {
-		mgo.Comm_Write(write)
-		time.Sleep(pause_dur)
-	}
+	// for _, write := range writes {
+	// 	mgo.Comm_Write(write)
+	// 	time.Sleep(pause_dur)
+	// }
 
-	stop_reading = true
+	// stop_reading = true
 
-	mgo.Comm_Disconnect()
+	// mgo.Comm_Disconnect()
 }
